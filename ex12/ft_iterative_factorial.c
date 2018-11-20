0ad8d6029eb704d9e69bb7b7b502eb4d1778c1e8
Watty Ballabar <wballaba@e2r1p2.21-school.ru>
2018-11-20 15:39:01 +0300

    ex12

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -10,4 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+int		ft_iterative_factorial(int nb)
+{
+	int fact;
+	int i;
 
+	i = 1;
+	fact = 1;
+	if (nb >= 0)
+	{
+		while (i <= nb)
+		{
+			fact = fact * i;
+			i++;
+		}
+		return (fact);
+	}
+	else
+		return (0);
+}
