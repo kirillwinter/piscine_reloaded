18be02e41acf5ff65292a4648cc266d31b7e0a64
Watty Ballabar <wballaba@e2r1p2.21-school.ru>
2018-11-20 16:44:39 +0300

    ex12

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -1,31 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wballaba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/20 15:17:42 by wballaba          #+#    #+#             */
-/*   Updated: 2018/11/20 15:17:45 by wballaba         ###   ########.fr       */
+/*   Created: 2018/11/20 16:34:13 by wballaba          #+#    #+#             */
+/*   Updated: 2018/11/20 16:34:15 by wballaba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_iterative_factorial(int nb)
-{
-	int fact;
-	int i;
 
-	i = 1;
-	fact = 1;
-	if (nb >= 0 && nb <=32)
-	{
-		while (i <= nb)
-		{
-			fact = fact * i;
-			i++;
-		}
-		return (fact);
-	}
-	else
-		return (0);
-}
