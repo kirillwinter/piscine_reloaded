6b23b3d4b40002a41a27cf6fd9657053c4ecf8cf
Watty Ballabar <wballaba@e2r1p5.21-school.ru>
2018-11-21 15:15:34 +0300

    ex19

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -43,20 +43,42 @@ int		ft_strcmp(char *s1, char *s2)
 	return (0);
 }
 
-int		main(int argc, char *argv[])
+void	ft_out(int argc, char **argv)
 {
-	int flag;
-	
-	if (argc == 3)
-	{ 
-		flag = ft_strcmp(argv[1], argv[2]);
-		if (flag > 0)
-		{
-			ft_putstr(argv[2]);
-		}
-		else 
-			ft_putstr(argv[1]);
+	int i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
 	}
-	return (0);
 }
 
+int		main(int argc, char *argv[])
+{
+	int		flag;
+	int		i;
+	char	*arr;
+
+	flag = 0;
+	while (flag == 0)
+	{
+		flag = 42;
+		i = 1;
+		while (i < argc - 1)
+		{
+			if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+			{
+				arr = argv[i];
+				argv[i] = argv[i + 1];
+				argv[i + 1] = arr;
+				flag = 0;
+			}
+			i++;
+		}
+	}
+	ft_out(argc, argv);
+	return (0);
+}
